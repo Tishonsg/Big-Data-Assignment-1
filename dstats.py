5315e9c96ba2d9bd662e448e8c21f27f9f80f100
Seansg <tishonsg@gmail.com>
2018-10-15 20:16:16 -0400

    Updates and Modifications
    dstats - Basic statistical procedures performed on song collections
    artist-profiling & song-profiling - Implementation of tf-idf (term frequency, inverse document frequency)

--- dstats.py
+++ dstats.py
@@ -1,5 +1,9 @@
 import csv;
+import matplotlib.pyplot as plt;
+import operator;
 from collections import Counter;
+from collections import OrderedDict;
+
 
 '''VARIABLE DECLARATIONS'''
 
@@ -9,7 +13,7 @@ artist_list = []
 # Loop id to identify location of artist after each iteration
 art_lp_id = 0;
 
-'''Song varibales'''
+'''Song variables'''
 song_list = []
 song_set = set()
 song_lp_id = 1;
@@ -18,45 +22,164 @@ song_lp_id = 1;
 link_set = set()
 
 '''Text variables'''
+text_list = []
 text_set = set()
+text_lp_id = 3;
+
 
 '''FINAL RESULTS'''
 numOfArtist = 0;
 numOfSongs = 0;
 avgNumOfSongs = 0;
 avgNumOfWords = 0;
+pairsOfArtistAvgNumOfWords = {};
 
+
+'''FUNCTION DECLARATIONS'''
+
+
+# Return the number of unique words per song in array list
+def unique_words(listOfSongs):
+    # Variables
+    uniqueWordSet = set();  # Unique words with in a single song
+    numOfWords = 0;         # Numerical value of unique words in a song
+    wordsPerSong = [];      # Number of words per song
+
+    for songLyrics in listOfSongs:
+        for words in songLyrics.split():
+            uniqueWordSet.add(words)
+        numOfWords = len(uniqueWordSet)
+        uniqueWordSet.clear()
+        wordsPerSong.append(numOfWords)
+
+    print(wordsPerSong)
+    return wordsPerSong
+
+
+''' <<<<<<<<<<<<<<<<<<< MAIN FUNCTION >>>>>>>>>>>>>>>>>> '''
 
 # OPEN CSV FILE
 with open('songdata.csv','r') as csv_file:
     songs = csv.reader(csv_file,delimiter=',')
+
+    # Skip the header of the csv
+    next(songs, None)
+
     # Row contains: artist, song, link and text
-
     for row in songs:
-            # Extract artist entries into an array
-            artist_list.append(row[art_lp_id])
-            # Extract song entries into an array
-            song_list.append(row[song_lp_id])
 
-    # Utilize a set to eliminate duplicate entries
+        # Extract artist entries into an array
+        artist_list.append(row[art_lp_id])
+        # Extract song entries into an array
+        song_list.append(row[song_lp_id])
+        # Extract song lyrics
+        text_list.append(row[text_lp_id])
+
+    # Utilize a set to eliminate duplicate entries(artist)
     for artist in artist_list:
         artist_set.add(artist)
+    artist_set = sorted(artist_set)
+
 
     # Determining number of Artist
     # Assignment of number of Artist
     numOfArtist = len(artist_set)
 
+
+
     # Determining number of Songs
     # Assignment of number of Songs
     numOfSongs = len(song_list)
 
+
     # Determining the average number of songs per artist
-    # Counter returns a key value pair of the number of occurences of duplicates in a list
+    # Counter returns a key value pair of the number of occurrences of duplicates in a list
     songsOfArtist = Counter(artist_list);
     avgNumOfSongs = (numOfSongs // numOfArtist)
 
     # Determining the average number of words per song
 
+    ttlNumOfWords = 0;
+    # For each collection of song lyrics loop through each word and append to a set to eliminate duplicates
+    # Keep a 'static' variable to count words in the set, and after the completion of a song read clear set and repeat
+    for lyrics in text_list:
+        for word in lyrics.split():
+            text_set.add(word)
+        ttlNumOfWords = ttlNumOfWords + len(text_set)
+        text_set.clear()
+    # Assignment of average number of unique words per song
+    avgNumOfWords = (ttlNumOfWords // numOfSongs)
+
+    # Determining the average number of unique words per song of an artist (arranged in ascending order of artist)
+
+    # Required Variables
+    songsPerArtist = []         # List of songs produced by an artist/band
+    wordsPerSongPerArtist = []  # List of words per each song produced by an artist
+    artistAvgWords = {}         # Dictionary of artist and the average number of words in their respective songs
+    allArtistWords = 0          # Total number of of words in across all songs produced by an artist
+    avgWordCount = 0;
+
+    for artist in artist_set:
+        print(artist)
+        csv_file.seek(0)
+        for record in songs:
+            if record[art_lp_id] == artist:
+                # Create a list of the lyrics of all songs produced by a unique artist
+                songsPerArtist.append(record[text_lp_id])
+        # Receive an array of the number of unique words in each song produced by an artist
+        wordsPerSongPerArtist = unique_words(songsPerArtist)
+
+        # Sum total number of words across all songs produced by an artist
+        for wordCount in wordsPerSongPerArtist:
+            allArtistWords = allArtistWords + int(wordCount)
+
+        # Determine average word count of a song produced by a artist
+        if allArtistWords > 0 and len(songsPerArtist) > 0:
+            avgWordCount = (allArtistWords //len(songsPerArtist))
+            # Append artist and average word count as tuple to dictionary
+            artistAvgWords.update({artist:avgWordCount})
+
+            # Reset variables to default state before iterating on next artist
+            songsPerArtist.clear()
+            allArtistWords = 0
+
+    # Sorting artistWord by Key
+    pairsOfArtistAvgNumOfWords = OrderedDict(sorted(artistAvgWords.items()))
+
+    # Graph Creation
+    # Plotting a table utilizing the math library library
+    # Where the x-axis represents the artists and the y-axis represents the average number of words.
+
+    '''BAR CHART VARIABLES'''
+    top = 10;  # number of permitted table entries in the bar graph
+    # Sort pairsOfArtistAvgNumOfWords on the value in descending order(desc -> (reverse=True))
+    orderedByValue = dict()
+    orderedByValue = sorted(pairsOfArtistAvgNumOfWords.items(), key=operator.itemgetter(1),reverse=True)[:top];
+
+    # x-coordinates(artist)
+    topAxis_x = []
+    for key in orderedByValue:
+       topAxis_x.append(key[0])
+
+    # y-coordinates  (average number of words)
+    topAxis_y = []
+    for value in orderedByValue:
+        topAxis_y.append(value[1])
+
+    # BAR CHART ATTRIBUTE SPECIFICATIONS
+
+    # Labels for bars
+    tick_label = topAxis_x
+    # Plotting a bar chart
+    plt.bar(topAxis_x, topAxis_y, width=0.5, color='green')
+    # naming the x-axis
+    plt.xlabel('Artist or Band')
+    # naming the y-axis
+    plt.ylabel('Average Number of Words')
+    # plot title
+    plt.title('Billboard Top 10')
+    # function to show the plot
+    plt.show()
 
 
 
---
+++ songprofiling.py
@@ -0,0 +1,131 @@
+import csv;
+import math;
+import operator;
+
+'''VARIABLE DECLARATIONS'''
+
+'''Text variables'''
+text_list = []
+lyrics_list =[]
+text_lp_id = 3;
+
+'''Complete list of songs words'''
+all_Songs = []
+dictList = []
+tfList = []
+idfList = []
+tfIdfFinal = []
+
+
+'''FUNCTION DEFINITION'''
+
+# This function takes in a list of dictionaries{word:wordCount} and a list of words
+# It then determines the frequency of  a word in a dictionary (wordCount/ ttlWords)
+# Creates a new dictionary list with key being word and value being Frequency of word
+
+
+def computeTF(wordDict,docWords):
+    tfDict = {}
+    docCount = len(docWords)
+    for word,count in wordDict.items():
+        tfDict[word] = count/float(docCount)
+    return tfDict
+
+
+# This function take in a list of dictionary {word:wordCount} and determines inverse frequency across all songs
+def computeIDF(songList):
+    tfDictList = []
+    listLen = len(songList)
+
+    # For every song identify unique words
+    for song in songList:
+        idfDict = dict.fromkeys(song.keys(), 0)
+        # Identify if the word occurs across  all song lyrics
+        for lyrics in songList:
+            for word, val in lyrics.items():
+                # Conditional statement to ensure that word exists in present song evaluated
+                if val>0 and word in idfDict:
+                    idfDict[word] = idfDict[word] + 1
+
+        for word,val in idfDict.items():
+            # Apply the formula for idf
+            idfDict[word] = math.log10(listLen/float(val))
+        tfDictList.append(idfDict)
+    return tfDictList
+
+
+def computeTFIDF(tfDocWords,idfs):
+    tfidf = {}
+    for word,val in tfDocWords.items():
+        tfidf[word] = val * idfs[word]
+    return tfidf
+
+
+''' <<<<<<<<<<<<<<<<<<< MAIN FUNCTION >>>>>>>>>>>>>>>>>> '''
+
+# OPEN CSV FILE
+with open('samplesongdata.csv','r') as csv_file:
+    songs = csv.reader(csv_file,delimiter=',')
+    # Skip the header of the csv
+    next(songs,None)
+
+    for row in songs:
+        # NOTE A COMPLETE  LIST OF STOP WORDS MUST BE MADE RATHER THAN HAVING MULTIPLE REPLACE STATEMENTS
+        # STOP WORDS LIST TO BE REFINED
+        line = row[text_lp_id].replace('\n','')
+        line = line.replace(',','')
+        line = line.replace('?','')
+        line = line.replace('[','')
+        line = line.replace(']','')
+        line = line.replace('(','')
+        line = line.replace(')','')
+        line = line.replace('\t','')
+        # Consider changing all words to their  lowercase equivalent
+        text_list.append(line)
+
+    # Parse each document in the text list by word
+    for lyrics in text_list:
+        lWords = lyrics.split()
+        all_Songs.append(lWords)
+    # Convert the list of words to a set of words to eliminate duplicates
+    for lyricWords in all_Songs:
+        lyricsSet = set(lyricWords)
+        lyricsSet = sorted(lyricsSet)
+        # Create a dictionary of unique words and their respective word count
+        wordDict = dict.fromkeys(lyricsSet,0)
+
+        # Iterative counter identifier for occurrences of a single word
+        for word in lyricWords:
+            wordDict[word]= wordDict[word] + 1
+        # Append dictionary pairing(word,wordCount) to list
+        dictList.append(wordDict.copy())
+        wordDict.clear()
+
+    # WARNING UTILIZING ZIP FOR ITERATING THROUGH LISTS SIMULTANEOUSLY IS NOT FEASIBLE FOR SCALABILITY
+
+    # Determine the total frequency score of each dictionary pairing in the list
+    for tupleList,wordsSong in zip(dictList,all_Songs):
+        tempTF = computeTF(tupleList,wordsSong)
+
+        tfList.append(tempTF)
+
+    # Determine IDF across all songs of a given word
+    idfList = computeIDF(dictList)
+
+    # Determine final Results (tf-idf) for each song
+    for idf,tf in zip(idfList,tfList):
+        idfList.append(computeTFIDF(tf,idf))
+
+    # VARIABLES FOR FORMATTING
+    sortTfIdf = []
+    top = 50
+
+    # FORMAT SPECIFICATIONS
+
+    for listing in idfList:
+        orderedByTfIdf = dict()
+        orderedByTfIdf = sorted(listing.items(), key=operator.itemgetter(1), reverse=True)[:top];
+        sortTfIdf.append(orderedByTfIdf)
+
+    for tfidf in sortTfIdf:
+        print(tfidf)
